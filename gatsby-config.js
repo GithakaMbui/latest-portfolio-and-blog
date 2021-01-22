5e47dd7b54ec80e697e414c133609aa0cf38e87a
Githaka <githakamaru@gmail.com>
2021-01-22 17:42:32 +0300

    created a featrured projects component

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,8 +8,8 @@ module.exports = {
   siteMetadata: {
     title: "WebDev Portfolio",
     description: "This is WebDev Portfolio Site",
-    author: "@webdev",
-    twitterUsername: "@john_smilga",
+    author: "@mbuideveloper",
+    twitterUsername: "@mbuideveloper",
     image: "/twitter-img.png",
     siteUrl: "https://testing-strapi-gatsby-build.netlify.app",
   },
@@ -33,7 +33,7 @@ module.exports = {
         //   contentTypes : `jobs`, `projects`, `blogs`,
         //   singleType : `about`
         //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
-        contentTypes: [`jobs`],
+        contentTypes: [`jobs`,`projects`],
         singleTypes: [],
       },
     },
